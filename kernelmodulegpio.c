061ad08388ffdf936c548376080d616bdb7ed01a
arne-vanmarcke <r0783508@student.vives.be>
2021-12-19 15:54:14 +0100

    Added timers

--- kernelmodulegpio.c
+++ kernelmodulegpio.c
@@ -1,6 +1,8 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/gpio.h>
+#include <linux/sched.h>
+#include <linux/timer.h>
 #include <linux/moduleparam.h>
 #include <linux/init.h>
 #include <linux/stat.h>
@@ -16,6 +18,8 @@ char led_name[20]="Led 24";
 const char *ptrLedName=&led_name[0];
 
 struct gpio leds[(sizeof outputs / sizeof (int))];
+struct timer_list blink_timer;
+struct timer_list blink_timer_2;
 
 module_param_array(outputs, int, &arr_argc, 0000);
 MODULE_PARM_DESC(outputs, "Set gpio's");
@@ -23,6 +27,32 @@ module_param_array(level, int, &arr_argc, 0000);
 MODULE_PARM_DESC(level, "Set gpio levels");
 module_param_array(togglespeed, int, &arr_argc, 0000);
 MODULE_PARM_DESC(togglespeed, "Set gpio togglespeed");
+
+static void blink_timer_func(struct timer_list* t)
+{
+	printk(KERN_INFO "%s\n", __func__);
+
+	gpio_set_value(outputs[0], level[0]);
+	level[0]^=1;
+	
+	/* schedule next execution */
+	//blink_timer.data = !data;						// makes the LED toggle 
+	blink_timer.expires = jiffies + (togglespeed[0]*HZ); 		// 1 sec.
+	add_timer(&blink_timer);
+}
+
+static void blink_timer_2_func(struct timer_list* t)
+{
+	printk(KERN_INFO "%s\n", __func__);
+
+	gpio_set_value(outputs[1], level[1]);
+	level[1]^=1;
+	
+	/* schedule next execution */
+	//blink_timer.data = !data;						// makes the LED toggle 
+	blink_timer_2.expires = jiffies + (togglespeed[1]*HZ); 		// 1 sec.
+	add_timer(&blink_timer_2);
+}
 
 static int __init kernelmodule_init(void){
     int i;
@@ -44,12 +74,43 @@ static int __init kernelmodule_init(void){
 	if (ret) {
 		printk(KERN_ERR "Unable to request GPIOs: %d\n", ret);
 	}
+
+    if(togglespeed[0]!=0){
+        timer_setup(&blink_timer, blink_timer_func, 0);
+        blink_timer.function = blink_timer_func;
+        //blink_timer.data = 1L;							// initially turn LED on
+        blink_timer.expires = jiffies + (togglespeed[0]*HZ); 		// 1 sec.
+        add_timer(&blink_timer);
+    }
+    if(togglespeed[1]!=0){
+        timer_setup(&blink_timer_2, blink_timer_2_func, 0);
+        blink_timer_2.function = blink_timer_2_func;
+        //blink_timer.data = 1L;							// initially turn LED on
+        blink_timer_2.expires = jiffies + (togglespeed[1]*HZ); 		// 1 sec.
+        add_timer(&blink_timer_2);
+    }
+
+    // for (i = 0; i < (sizeof outputs / sizeof (int)); i++)
+    // {
+    //     if(togglespeed[i]!=0){
+    //         index=i;
+    //         timer_setup(&blink_timer[i], blink_timer_func, 0);
+    //         blink_timer[i].function = blink_timer_func;
+    //         //blink_timer.data = 1L;							// initially turn LED on
+    //         blink_timer[i].expires = jiffies + (togglespeed[i]*HZ); 		// 1 sec.
+    //         add_timer(&blink_timer[i]);
+    //     }
+    // }
+
     
     return 0;
 }
 static void __exit kernelmodule_exit(void)
 {
     int i;
+    
+    del_timer_sync(&blink_timer);
+    del_timer_sync(&blink_timer_2);
 
     for(i = 0; i < ARRAY_SIZE(leds); i++) {
 		gpio_set_value(leds[i].gpio, 0); 
